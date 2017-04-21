1129a5c2184e6149c62229638332d5843d37d8b4
jlee512 <jlee512@aucklanduni.ac.nz>
2017-04-21 17:01:46 +1200

    all progress 21/04/2017

--- src/WoodleJumpViewer.java
+++ src/WoodleJumpViewer.java
@@ -48,21 +48,34 @@ public class WoodleJumpViewer extends JPanel implements ActionListener {
         //Get person to do first jump automatically
 
         person.jump(0, 400,800);
+        System.out.println(person.getMidY());
         platformID ++;
         repaint();
 
         //Loop through array of platforms
         for (int platformID = 0; platformID < platforms.size(); platformID++) {
+            System.out.println("PlatformId: " + platformID);
+            System.out.println(person.getDeltaY());
             int platformY = 800 - platforms.get(platformID).getY();
             int platformX = platforms.get(platformID).getX();
-            while (!person.initialiseJump(platformX, platformX + 40, platformY)) {
-                System.out.println(person.getDeltaY());
-                System.out.println(!person.initialiseJump(platformX, platformX + 40, platformY));
-                System.out.println(person.getMidY());
+            int i = 0;
+            //Set speed to initial speed because we have hit a platform
+            while (!person.initialiseJump(platformX, platformX + 40, platformY) && i < 15) {
+                System.out.println("midY initial: " + person.getMidY());
+                System.out.println("Plat Y:" + platformY);
+
                 person.jump(platformX, platformX + 40, platformY);
                 repaint();
-                
+                if(person.getMidY() == platformY && person.getDeltaY() <= 0){
+                    person.setDeltaY(person.INITIAL_DELTA_Y);
+                }
+                System.out.println("midY final: " + person.getMidY());
+                System.out.println(person.initialiseJump(platformX, platformX + 40, platformY));
+                i++;
+
             }
+
+
         }
     }
 
@@ -71,6 +84,7 @@ public class WoodleJumpViewer extends JPanel implements ActionListener {
             @Override
             public void run(){
                 WoodleJumpFrame frame = new WoodleJumpFrame();
+                frame.setVisible(true);
             }
         });
     }
