cc0cfccc0205a611c3d71415f48a67b97d0e7a93
jlee512 <jlee512@aucklanduni.ac.nz>
2017-04-21 13:13:11 +1200

    person second commit - first jump sequence with generic inputs for platform dimensions

--- src/Person.java
+++ src/Person.java
@@ -9,20 +9,16 @@ public class Person {
     private int y;
     //Set deltaX to zero initially for testing simplicity
     private int deltaX = 0;
-    private int deltaY;
-    private int midX;
-    private int midY;
+    private int deltaY = 0;
     public static final int INITIAL_DELTA_Y = 20;
     public static final int JUMP_HEIGHT = 60;
     public static final int GRAVITY = 4;
 
-    public Person(int deltaX, int deltaY) {
+    public Person() {
         this.width = 10;
         this.height = 15;
-        this.x = 190;
+        this.x = 25;
         this.y = 785;
-        this.deltaX = deltaX;
-        this.deltaY = deltaY;
     }
 
     public int getX() {
@@ -49,16 +45,16 @@ public class Person {
         return (y + height);
     }
 
-    public boolean initialiseJump (int midY, int midX) {
-        if ((midX >= 10 || midX <= 50) && (midY == 780 || midY == 800) && (deltaY <= 0)) {
+    public boolean initialiseJump (int platformX1, int platformX2, int platformTopY) {
+        if ((getMidX() >= platformX1 && getMidX() <= platformX2) && (getMidY() == platformTopY) && (deltaY <= 0)) {
             return true;
         }
         return false;
     }
 
-    public void jump (){
+    public void jump (int platformX1, int platformX2, int platformTopY){
         //Set speed at time click
-        if(initialiseJump(getMidX(), getMidY())){
+        if(initialiseJump(platformX1, platformX2, platformTopY)){
             //If beginning to jump set initial speed to 20
             deltaY = INITIAL_DELTA_Y;
             deltaX = 0;
@@ -69,11 +65,36 @@ public class Person {
         }
 
         //Calculate next position at time click
-        int nextX = x + deltaX;
-        int nextY = y + deltaY;
+        int nextX = x - deltaX;
+        int nextY = y - deltaY;
+        if(nextY > platformTopY){
+            nextY = platformTopY;
+        }
 
         //Set position for next time click
         setX(nextX);
         setY(nextY);
+    }
+
+    public static void main(String[] args) {
+        //Person mid point calculation works
+        Person guy = new Person();
+        int midX = guy.getMidX();
+        System.out.println(midX);
+        int midY = guy.getMidY();
+        System.out.println(midY);
+
+        //Initialise jump works
+        System.out.println(guy.initialiseJump(10, 50, 800));
+
+        //Test jump to apex
+        for (int i = 0; i < 10; i++) {
+            guy.jump(10, 50, 800);
+            System.out.println("Jump completed, positions:");
+            System.out.println(guy.getMidX());
+            System.out.println(guy.getMidY());
+        }
+        //Check initialise jump on second platform
+        System.out.println(guy.initialiseJump(10, 50, 780));
     }
 }
