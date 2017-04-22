9edf40971df8b76f4361f56199ad1bc5e7f253b2
ljam763 <ljam763@aucklanduni.ac.nz>
2017-04-22 20:16:15 +1200

    "Fixed the person jump within person class and within testing1 "

--- src/Person.java
+++ src/Person.java
@@ -9,9 +9,10 @@ public class Person {
     private int height;
     private int x;
     private int y;
+    private int i =0;
     //Set deltaX to zero initially for testing simplicity
     private int deltaX = 0;
-    private int deltaY = 0;
+    private int deltaY = 20;
     private boolean stillRising = true;
     private boolean onNextPlatform = false;
 
@@ -67,88 +68,71 @@ public class Person {
         return deltaY;
     }
 
-    public boolean alignedWithPlatform(int platformX1, int platformX2){
-        if(getMidX() >= platformX1 && getMidX() <= platformX2){
+//    public boolean alignedWithPlatform(int platformX1, int platformX2){
+//        if(getMidX() >= platformX1 && getMidX() <= platformX2){
+//            return true;
+//        }
+//        return false;
+//    }
+    public boolean jump (int startingPoint, int platefromY){
+        System.out.println("Speed " + deltaY);
+
+        if(deltaY <= 0 && this.getY() <= platefromY){
+            return falling(platefromY);
+        }
+        else if (deltaY <= 0 && !(this.getY() <= platefromY)){
+            System.out.println("Fall jump can't reach at the end needed " + (this.getY() - platefromY));
+            return false;
+        }
+        if(startingPoint == this.getMidY()){
+            deltaY = INITIAL_DELTA_Y;
+            setY(this.y - deltaY);
+            System.out.println(this.y + " Rising");
+        }
+        setY(this.y - deltaY);
+        System.out.println(this.y + " Rising");
+        deltaY -= GRAVITY;
+        return jump(startingPoint, platefromY);
+    }
+
+    public boolean falling(int platefromY){
+        deltaY -= GRAVITY;
+        System.out.println("Speed " + deltaY);
+        System.out.println("Estimate Y " + (this.y - deltaY));
+        setY(this.y - deltaY);
+        System.out.println(this.y+ " Dropping");
+        if (this.y >= platefromY){
+            System.out.println("Speed " + deltaY);
+            System.out.println("Estimate Y " + (this.y - deltaY));
+            setY(platefromY);
+            System.out.println("Current Y " + this.y);
+            System.out.println("Success? Clear");
+            deltaY = INITIAL_DELTA_Y;
             return true;
         }
-        return false;
+        System.out.println("not clear yet still have"+ (this.y- platefromY));
+        return falling(platefromY);
     }
 
-    //The initialise jump function sets the initial speed when the person has fallen on top of a platform
-    public void initialiseJumpSpeed () {
-        //set speed to the initial speed
-        deltaY = INITIAL_DELTA_Y;
-        deltaX = 0;
-    }
 
-//    public boolean jumpRise(){
-//        //Calculate next position and set next position
-//        int nextX = x - deltaX;
-//        int nextY = y - deltaY;
-//        setY(nextY);
-//        setX(nextX);
-//
-//        //Reduce Y-speed by gravity function, alter X-speed accordingly (to be confirmed at later stage)
-//        deltaY -= GRAVITY;
-//        deltaX -= 0;
-//
-//        //Return if person is still rising as a boolean
-//        if(deltaY <= 0){
-//          return false;
-//        }
-//        return true;
-//    }
-
-    public boolean jumpMove(int platformX1, int platformX2, int platformTopY){
-        //Calculate next position and set next position
-        int nextX = x - deltaX;
-        int nextY = y - deltaY;
-        //Check if falling and
-
-
-        setY(nextY);
-        setX(nextX);
-
-        //Reduce Y-speed by gravity function, alter X-speed accordingly (to be confirmed at later stage)
-        deltaY -= GRAVITY;
-        deltaX -= 0;
-
-        //Return if person is on top of next platform
-        if(get)
-    }
-
-    public void jump (int platformX1, int platformX2, int platformTopY){
-        //Set speed at time click
-        if(initialiseJump(platformX1, platformX2, platformTopY)){
-            //If beginning to jump set initial speed to 20
-            deltaY = INITIAL_DELTA_Y;
-            deltaX = 0;
-        } else {
-            //Set to reduced speed
-            deltaY -= GRAVITY;
-            deltaX = 0;
-        }
-
-        //Calculate next position at time click
-
-
-        //Set position for next time click
-        setX(nextX);
-        setY(nextY);
-    }
 
     public static void main(String[] args) {
         //Person mid point calculation works
         Person guy = new Person();
+        //more than max jump at first from 785 to 720 = 65;
+//        int[] plates = {720, 685, 640, 620};
+        //less than max jump at first from 785 to 730 = 55;
+        int[] plates = {730, 685, 640, 620};
+        for (int plate : plates) {
+            if (!guy.jump(guy.getY(), plate)){
+                break;
+            }
+            else{
+                System.out.println(true);
+            }
+        }
 
         //Test jump to apex
-        for (int i = 0; i < 10; i++) {
-            guy.jump(10, 50, 800);
-            System.out.println("Jump completed, positions:");
-            System.out.println(guy.getMidX());
-            System.out.println(guy.getMidY());
-            System.out.println(guy.initialiseJump(10, 50,780));
-        }
     }
 
     public void paint (GraphicsPainter painter){
